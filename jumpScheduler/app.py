a6d8ed3bf1022c70307ee81ffcd84eabad9e005a
Elijah Retzlaff <elijahretzlaff@macbook-air.myfiosgateway.com>
2022-11-09 20:18:12 -0500

    First commit

---
+++ jumpScheduler/app.py
@@ -0,0 +1,17 @@
+# jumpScheduler/app.py
+
+""" Flask website application for the Jump Scheduler software
+
+Authors: Theo Shapiro, Elijah Retzlaff
+"""
+
+import flask
+
+# 1 - Create virtual env 
+
+# 2 - Create file structure 
+
+# 3 - Init Git
+
+# 4 - Create requirements . txt 
+
